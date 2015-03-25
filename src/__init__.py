0adf5987b91c731301d696d33e0ee55df902cf44
Steve Schmerler <mefx@gmx.net>
2015-03-25 12:53:01 +0100

    INT: Move tools.py to src/ and incorporate into src/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,20 @@ ffnet package
 
 from _version import version
 import fortran
-import ffnet as ffnetmodule
+
+# elcorto 2015-03: Don't need this, eh?
+##import ffnet as ffnetmodule
+
+# elcorto 2015-03: Why?? This kind of aliasing makes people go nuts when they
+# try to understand a new package. ffnet.ffnet.foo == ffnet.foo, what?? Better
+# separate functions into modules such as
+#   ffnet.graph.mlgraph
+#   ffnet.graph.tmlgraph
+#   ffnet.graph.imlgraph
+#   ffnet.io.savenet
+#   ffnet.io.loadnet
+#   ffnet.io.exportnet
+#   ffnet.io.readdata
 from ffnet import ffnet, \
                   mlgraph, \
                   tmlgraph, \
@@ -15,5 +28,7 @@ from ffnet import ffnet, \
                   loadnet, \
                   exportnet, \
                   readdata
+                  
+import tools
 from pikaia import pikaia
 import _tests
